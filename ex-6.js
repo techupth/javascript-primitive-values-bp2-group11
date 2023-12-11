1d626a8747648d912ea64d73f2b837d8e9212572
Phuree Limphornpugdee <phureelimphornpugdee@Phurees-MacBook-Air.local>
2023-12-11 21:27:34 +0700

    feat: excercise primitive value by Tar

--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,7 @@
 // Exercise #6: Displaying message
 
-let totalParts = 13000;
+let totalParts = 13000; // Total: 13000 Machine parts
 
-let concatSentence;
-let literalSentence;
+let concatSentence = "Total:"+ " " + totalParts +" Machine parts";
+let literalSentence = `Total: ${totalParts} Machine parts`;
+
